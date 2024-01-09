332b4b33005e2b6f2b331ac0a6428aa3c9565443
AbubakrKane <bkanej234@gmail.com>
2024-01-09 19:25:07 +0100

    ramita

--- Ahorcado.js
+++ Ahorcado.js
@@ -15,3 +15,51 @@ let palabraConGuiones = palabra.replace(/./g,"_ ");
 
 let contadorFallos=0;
 
+//<p> id output sera reemplazado por "palabraConGuiones"
+document.querySelector('#output').innerHTML = palabraConGuiones;
+button.addEventListener('click',()=>{
+    
+    //obtener el valor de la letra ingresada en id letra y guardarlo en la const letra.
+    let letra=document.querySelector('#letra').value;
+    letra=letra.toLowerCase();
+    let haFallado=true;
+    for(const i in palabra){
+        //si encuentra la letra, reemplazara el guión+espacio por la letra que encontro en el espacio en que lo encontro.
+        if(letra==palabra[i]){
+            palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra);
+            haFallado=false;
+        }
+    }
+
+    /* condición de ganador o perdedor */
+    if(haFallado){
+       contadorFallos++;
+       document.querySelector('#ahorcado').style.backgroundPosition= -(180*contadorFallos)+'px 0px'; 
+       if(contadorFallos>2){
+        document.querySelector('#ahorcado').style.backgroundPosition= -(198*contadorFallos)+'px 220px';
+       }
+       if(contadorFallos>=5){
+        document.querySelector('#resultado').innerHTML="¡Eres un Loser!";
+        document.getElementById('resultado').style.backgroundColor="#740707";
+        document.getElementById('resultado').style.color = "#ffffff";
+
+        
+        button.disabled=true;
+       }
+    }else{
+        if(palabraConGuiones.indexOf('_')<0){
+            document.querySelector('#resultado').innerHTML="¡¡Has Ganado!!";
+            document.getElementById('resultado').style.backgroundColor="#0f8117";
+            document.getElementById('resultado').style.color = "#ffffff";
+            button.disabled=true;
+        }
+    }
+
+    
+       
+       /* document.querySelector('#ahorcado').style.backgroundPosition= -(100*contadorFallos)+'px'; */
+    
+    /* reemplazar los datos de id output por la variable palabra con guiones */
+    document.querySelector('#output').innerHTML=palabraConGuiones;
+    
+});
